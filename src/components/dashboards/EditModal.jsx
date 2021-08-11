12acee476ee72af6945308483d046981fa304583
Nathaniel Friedman <nathaniel.friedman@duke.edu>
2021-08-11 17:14:56 -0400

    added react imports

--- src/components/dashboards/EditModal.jsx
+++ src/components/dashboards/EditModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Modal, Col, Input, Select } from 'antd';
 import { widgetDict } from './Constants';
 import { GetDataById } from '../../api/api';
